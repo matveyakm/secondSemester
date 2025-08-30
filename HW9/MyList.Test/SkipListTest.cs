0ae22dbf310cd63dce85b102495fc937dd71c048
matveyakm <matveyakm@gmail.com>
2025-08-30 22:47:47 +0300

    tests

--- HW9/MyList.Test/SkipListTest.cs
+++ HW9/MyList.Test/SkipListTest.cs
@@ -38,8 +38,53 @@ public class SkipListTest
         testedList.Add("ivgwrrwtanw");
         testedList.Add("twenawtvwtitwt");
 
-
         Assert.That(testedList.Count, Is.EqualTo(expected));
+    }
+
+    /// <summary>
+    /// index of existing item.
+    /// </summary>
+    [Test]
+    public void SkipList_IndexOf()
+    {
+        var testedList = new SkipList<string>(["matvey", "akimenko", "cool"]);
+        var result = testedList.IndexOf("akimenko");
+
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    /// <summary>
+    /// enumerator should return all elements.
+    /// </summary>
+    [Test]
+    public void SkipList_GetEnumerator_ReturnsAllElements()
+    {
+        var tested = new SkipList<int>((int[])[124, 543, 893]);
+        var result = new List<int>();
+
+        foreach (var item in tested)
+        {
+            result.Add(item);
+        }
+
+        Assert.That(result, Is.EquivalentTo((int[])[124, 543, 893]));
+    }
+
+    /// <summary>
+    /// modification collection while iteration should throw exception.
+    /// </summary>
+    [Test]
+    public void SkipList_GetEnumerator_ThrowsInvalidOperationException()
+    {
+        var testedList = new SkipList<char>((char[])['y', 'w', 'z']);
+        var enumerator = testedList.GetEnumerator();
+
+        enumerator.MoveNext();
+        testedList.Add('r');
+
+        Assert.Throws<InvalidOperationException>(() => enumerator.MoveNext());
+
+        enumerator.Dispose();
     }
 
     /// <summary>
@@ -59,6 +104,61 @@ public class SkipListTest
     }
 
     /// <summary>
+    /// correct value of Count after clearing list.
+    /// </summary>
+    [Test]
+    public void SkipList_Clear_ShouldResetCountToZero()
+    {
+        var testedList = new SkipList<int>([1, 2, 3]);
+        Assert.That(testedList.Count, Is.EqualTo(3));
+
+        testedList.Clear();
+
+        Assert.That(testedList.Count, Is.EqualTo(0));
+    }
+
+    /// <summary>
+    /// skip list contains item.
+    /// </summary>
+    [Test]
+    public void SkipList_Contains_ShouldReturnTrue()
+    {
+        var testedList = new SkipList<int> { 100, 1135, 13421, 134 };
+
+        Assert.That(testedList.Contains(1135), Is.True);
+    }
+
+    /// <summary>
+    /// remove element.
+    /// </summary>
+    [Test]
+    public void SkipList_Remove_ExistingItem_ReturnsTrueAndRemovesItem()
+    {
+        var testedList = new SkipList<string>(["matvey", "akimenko"]);
+
+        var result = testedList.Remove("matvey");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.True);
+            Assert.That(testedList.Contains("matvey"), Is.False);
+            Assert.That(testedList.Count, Is.EqualTo(1));
+        });
+    }
+
+    /// <summary>
+    /// test for correct removing non-existing item.
+    /// </summary>
+    [Test]
+    public void SkipList_Remove_NonExistingItem_ReturnsFalse()
+    {
+        var list = new SkipList<int>([124, 232, 23523]);
+        var result = list.Remove(4);
+
+        Assert.That(result, Is.False);
+    }
+
+    /// <summary>
     /// add two thousand elements.
     /// </summary>
     [Test]
@@ -73,11 +173,4 @@ public class SkipListTest
 
         Assert.That(testedList.Contains(1001), Is.True);
     }
-
-    /// <summary>
-    /// add null item should throw argument null exception.
-    /// </summary>
-    [Test]
-    public void SkipList_Add_NullItem_ShouldThrowArgumentNullException()
-        => Assert.Throws<ArgumentNullException>(() => new SkipList<string>().Add(null!));
 }
